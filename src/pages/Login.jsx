e49ab66b087206be826801c058043c9a768d176f
mahojv <moisesmjavier@outlook.com>
2025-05-16 14:09:19 -0600

    feat: Update labels in Login form for clarity and consistency

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -32,11 +32,11 @@ export default function Login() {
                 <div className='  border border-[#00ebc7] px-[50px] py-[30px] rounded-2xl bg-gray-200 mt-8'>
                     <form className="max-w-sm mx-auto" onSubmit={handleLogin}>
                         <div className="mb-5">
-                            <label htmlFor="email" className="block mb-2 text-sm font-medium ">Your email</label>
+                            <label htmlFor="email" className="block mb-2 text-sm font-medium ">Email:</label>
                             <input type="email" id="email" name='email' className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@mail.com" required />
                         </div>
                         <div className="mb-5">
-                            <label htmlFor="password" className="block mb-2 text-sm font-medium ">Your password</label>
+                            <label htmlFor="password" className="block mb-2 text-sm font-medium ">Contraseña: </label>
                             <input type="password" id="password" name='password' className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Escribe tu contraseña" required />
                         </div>
                         
